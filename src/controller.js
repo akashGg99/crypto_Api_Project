72d311bde0ae9ac17be98606e3ee9b639a8b196f
akashGg99 <aakkash15r@gmail.com>
2022-12-08 23:51:17 +0530

    ssv

--- src/controller.js
+++ src/controller.js
@@ -5,16 +5,19 @@ import crypto_Model  from './model.js'
 
 const getcoins = async (req , res )=>{
     try {
+
         const option = {
             method : "GET" ,
             headers: {
                 Authorization: " Bearer 99ed61cb-0c4c-4c5c-a5ed-4a9b74b1a67c",
               } ,
-            url : "https://api.coincap.io/v2/assets",    
+            url : "https://api.coincap.io/v2/assets"
         }
 
         const result = await axios(option)
+
         const {data} = result.data
+        // console.log(data)
         data.sort((a,b)=> a.changePercent24Hr < b.changePercent24Hr)
 
         await crypto_Model.deleteMany()
